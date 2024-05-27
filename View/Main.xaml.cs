3337e2d77ca62d75a4885f9544d93daeffbaa39d
nightSanches <galesavich@gmail.com>
2024-05-27 18:19:40 +0500

    Реализованное приложение на оценку удовлетворительно. Савичев

--- View/Main.xaml.cs
+++ View/Main.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using pr41savichev.ViewModell;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -21,6 +22,7 @@ namespace pr41savichev.View
         public Main()
         {
             InitializeComponent();
+            DataContext = new VMStopwatch();
         }
     }
 }
